4f341c8aa97d585087789ef8cc19a1aefa04d222
Angus061855 <165797999+Angus061855@users.noreply.github.com>
2026-06-28 21:37:06 +0800

    Install new Meta Pixel site-wide

---
+++ meta-pixel.js
@@ -0,0 +1,36 @@
+(function (window, document) {
+  if (window.fbq) return;
+
+  var fbq = window.fbq = function () {
+    if (fbq.callMethod) {
+      fbq.callMethod.apply(fbq, arguments);
+    } else {
+      fbq.queue.push(arguments);
+    }
+  };
+
+  if (!window._fbq) window._fbq = fbq;
+  fbq.push = fbq;
+  fbq.loaded = true;
+  fbq.version = '2.0';
+  fbq.queue = [];
+
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://connect.facebook.net/en_US/fbevents.js';
+  var firstScript = document.getElementsByTagName('script')[0];
+  firstScript.parentNode.insertBefore(script, firstScript);
+
+  fbq('init', '1976183379740416');
+  fbq('track', 'PageView');
+
+  document.addEventListener('click', function (event) {
+    var link = event.target.closest('a[href]');
+    if (!link) return;
+
+    var href = link.getAttribute('href') || '';
+    if (href.indexOf('line.me') !== -1 || href.indexOf('lin.ee') !== -1) {
+      fbq('track', 'Contact', { channel: 'LINE' });
+    }
+  });
+})(window, document);
